b5f4557d9b623d1a745ef27d241c11a4cd9d8a53
Dennisovich-03 <54091928+Dennisovich-03@users.noreply.github.com>
2020-02-10 02:39:29 +0530

    v2

--- minmax.js
+++ minmax.js
@@ -1,24 +1,67 @@
-// function nextTurn() {
-//     let index = floor(random(available.length));
-//     let spot = available.splice(index, 1)[0];
-//     let i = spot[0];
-//     let j = spot[1];
-//     board[i][j] = players[currentplayer];
-//     currentplayer = (currentplayer + 1) % players.length;
-// }
-function nextTurn() {
-    let available = [];
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
+function bestMove() {
+    // AI to make its turn
+    let bestScore = -Infinity;
+    let move;
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            // Is the spot available?
             if (board[i][j] == '') {
-                available.push({
-                    i,
-                    j
-                })
+                board[i][j] = ai;
+                let score = minimax(board, 0, false);
+                board[i][j] = '';
+                if (score > bestScore) {
+                    bestScore = score;
+                    move = {
+                        i,
+                        j
+                    };
+                }
             }
         }
     }
+    board[move.i][move.j] = ai;
+    currentPlayer = human;
 }
-let move = random(available);
-board[move.i][move.j] = ai;
-currentplayer = human;
+
+let scores = {
+    X: 10,
+    O: -10,
+    tie: 0
+};
+
+function minimax(board, depth, isMaximizing) {
+    let result = checkWinner();
+    if (result !== null) {
+        return scores[result];
+    }
+
+    if (isMaximizing) {
+        let bestScore = -Infinity;
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                // Is the spot available?
+                if (board[i][j] == '') {
+                    board[i][j] = ai;
+                    let score = minimax(board, depth + 1, false);
+                    board[i][j] = '';
+                    bestScore = max(score, bestScore);
+                }
+            }
+        }
+        return bestScore;
+    } else {
+        let bestScore = Infinity;
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                // Is the spot available?
+                if (board[i][j] == '') {
+                    board[i][j] = human;
+                    let score = minimax(board, depth + 1, true);
+                    board[i][j] = '';
+                    bestScore = min(score, bestScore);
+                }
+            }
+        }
+        return bestScore;
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,89 +1,101 @@
+// Tic Tac Toe AI with Minimax Algorithm
+// The Coding Train / Daniel Shiffman
+// https://thecodingtrain.com/CodingChallenges/154-tic-tac-toe-minimax.html
+// https://youtu.be/I64-UTORVfU
+// https://editor.p5js.org/codingtrain/sketches/0zyUhZdJD
+
 let board = [
     ['', '', ''],
     ['', '', ''],
-    ['', '', ''],
+    ['', '', '']
 ];
 
+let tie = 0;
+let wins = 0;
+let losses = 0;
 
-let players = ['X', 'O'];
-let currentplayer;
-let available = [];
+let w; // = width / 3;
+let h; // = height / 3;
+
+let ai = 'X';
+let human = 'O';
+let currentPlayer = human;
 
 function setup() {
-    createCanvas(800, 800);
-    frameRate(1);
-    currentplayer = floor(random(players.length));
-    for (let j = 0; j < 3; j++) {
-        for (let i = 0; i < 3; i++) {
-            available.push([i, j]);
-        }
-    }
+    createCanvas(400, 400);
+    w = width / 3;
+    h = height / 3;
+    bestMove();
 }
 
 function equals3(a, b, c) {
-    return (a == b && b == c && a != '');
+    return a == b && b == c && a != '';
 }
 
 function checkWinner() {
     let winner = null;
 
-    // horizontal 
+    // horizontal
     for (let i = 0; i < 3; i++) {
         if (equals3(board[i][0], board[i][1], board[i][2])) {
             winner = board[i][0];
         }
     }
 
-    // vertical 
+    // Vertical
     for (let i = 0; i < 3; i++) {
         if (equals3(board[0][i], board[1][i], board[2][i])) {
             winner = board[0][i];
         }
     }
 
-    // diagonal 
-
+    // Diagonal
     if (equals3(board[0][0], board[1][1], board[2][2])) {
         winner = board[0][0];
     }
-
     if (equals3(board[2][0], board[1][1], board[0][2])) {
         winner = board[2][0];
     }
 
-    if (winner == null && available.length == 0) {
+    let openSpots = 0;
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (board[i][j] == '') {
+                openSpots++;
+            }
+        }
+    }
+
+    if (winner == null && openSpots == 0) {
         return 'tie';
+        tie++;
     } else {
         return winner;
     }
-
 }
 
-
-
-
 function mousePressed() {
-    if (currentplayer == human) {
+    if (currentPlayer == human) {
+        // Human make turn
         let i = floor(mouseX / w);
         let j = floor(mouseY / h);
+        // If valid turn
         if (board[i][j] == '') {
             board[i][j] = human;
-            currentplayer = ai;
-            nextTurn();
+            currentPlayer = ai;
+            bestMove();
         }
     }
 }
 
 function draw() {
-    background(220);
-    let w = width / 3;
-    let h = height / 3;
+    background(255);
+    strokeWeight(15);
+
     line(w, 0, w, height);
     line(w * 2, 0, w * 2, height);
-
     line(0, h, width, h);
     line(0, h * 2, width, h * 2);
-
 
     for (let j = 0; j < 3; j++) {
         for (let i = 0; i < 3; i++) {
@@ -91,24 +103,26 @@ function draw() {
             let y = h * j + h / 2;
             let spot = board[i][j];
             textSize(32);
-            strokeWeight(20);
-            if (spot == players[1]) {
+            let r = w / 4;
+            if (spot == human) {
                 noFill();
-                ellipse(x, y, w / 2);
-            } else if (spot == players[0]) {
-                let xr = w / 4;
-                line(x - xr, y - xr, x + xr, y + xr);
-                line(x + xr, y - xr, x - xr, y + xr);
+                ellipse(x, y, r * 2);
+            } else if (spot == ai) {
+                line(x - r, y - r, x + r, y + r);
+                line(x + r, y - r, x - r, y + r);
             }
         }
     }
-    let result = checkWinner();
 
+    let result = checkWinner();
     if (result != null) {
         noLoop();
-        createP(result).style('color', '#FFF').style('font-size', '32pt');
-
+        let resultP = createP('');
+        resultP.style('font-size', '75pt').style('text-align', 'center');
+        if (result == 'tie') {
+            resultP.html('Tie!');
+        } else {
+            resultP.html(`${result} wins!`);
+        }
     }
-    nextTurn();
-
 }
